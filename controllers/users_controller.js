6770853cc755cb17a58e9626aa9e82c9dc82c378
Om Sinkar <omsinkar03bit@gmail.com>
2023-06-06 13:37:00 +0530

    passport local sign in added.

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,43 +1,60 @@
 const User = require('../models/user');
-module.exports.profile = function(req, res){
+module.exports.profile = function (req, res) {
     return res.render('user_profile', {
         title: 'User Profile'
     })
 }
 
-module.exports.signIn = function(req, res){
+module.exports.signIn = function (req, res) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/users/profile');
+    }
+
     return res.render('Signin', {
         title: 'Codeial | Sign In'
     })
 }
 
-module.exports.signUp = function(req, res){
+module.exports.signUp = function (req, res) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/users/profile');
+    }
+
     return res.render('Signup', {
         title: 'Codeial | Sign Up'
     })
 }
 
-module.exports.create = function(req, res){
+module.exports.create = function (req, res) {
 
-    if(req.body.password != req.body.confirm_password){
+    if (req.body.password != req.body.confirm_password) {
         return res.redirect('back');
     }
-    User.findOne({email:req.body.email}, function(err, user){
-        if(err){console.log('errror in finding user in signing up'); return}
+    User.findOne({ email: req.body.email }, function (err, user) {
+        if (err) { console.log('errror in finding user in signing up'); return }
 
-        if(!user){
-            User.create(req.body, function(err, user){
-                if(err){console.log('error in creating user while sigining up'); return}
+        if (!user) {
+            User.create(req.body, function (err, user) {
+                if (err) { console.log('error in creating user while sigining up'); return }
                 return res.redirect('/users/sign-in')
             })
         }
-        else{
+        else {
             return res.redirect('back');
         }
     })
-    
+
 }
 
-module.exports.createSession = function(res, req){
+module.exports.createSession = function (req, res) {
+    return res.redirect('/')
+}
 
+module.exports.destorySession = function (req, res) {
+    req.logout(function (error) {
+        if (error) {
+            return next(error);
+        }
+        return res.redirect("/");
+    });
 }
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('./config/passport-local-strategy')
 
 app.use(express.urlencoded());
 app.use(cookieParser());
@@ -16,11 +18,26 @@ app.set('layout extractScript', true);
 
 //use express router
 app.use(expressLayouts);
-app.use('/', require('./routes'));
 
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+app.use(session({
+    name: 'codeial',
+    secret: 'fuckyoubitch',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000*60*100)
+    }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(passport.setAuthenticatedUser);
+
+
+app.use('/', require('./routes'));
 
 app.listen(port, function(err){
     if(err){
